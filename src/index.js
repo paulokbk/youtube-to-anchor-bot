0700d17d7fde7466fafd860f0c5febec92533199
Paulo Filho <paulokbkfilho@hotmail.com>
2023-02-22 23:41:58 -0300

    update: main function

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-const fs = require('fs');
-const { exit, title } = require('process');
+const fs = require("fs");
+const { exit, title } = require("process");
 
-const env = require('./environment-variables');
-const { getVideoInfo, downloadThumbnail, downloadAudio } = require('./youtube-yt-dlp');
-const { postEpisode } = require('./anchorfm-pupeteer');
+const env = require("./environment-variables");
+const {
+  getVideoInfo,
+  downloadThumbnail,
+  downloadAudio,
+} = require("./youtube-yt-dlp");
+const { postEpisode } = require("./anchorfm-pupeteer");
+const { CheckNewVideo, UpdateJSON } = require("./youtube-new-video");
 
 function validateYoutubeVideoIds(ids) {
   if (!Array.isArray(ids) || ids.length === 0) {
-    throw new Error('A lista de IDs está vazia ou não é um array');
+    throw new Error("A lista de IDs está vazia ou não é um array");
   }
 
   for (const id of ids) {
-    if (id === undefined || id === null || typeof id !== 'string') {
-      throw new Error('Há um ID inválido na lista');
+    if (id === undefined || id === null || typeof id !== "string") {
+      throw new Error("Há um ID inválido na lista");
     }
   }
 }
 
-
 function getYoutubeVideoIds() {
   try {
-    const json = JSON.parse(fs.readFileSync(env.EPISODE_PATH, 'utf-8'));
+    const json = JSON.parse(fs.readFileSync(env.EPISODE_PATH, "utf-8"));
     validateYoutubeVideoIds(json.ids);
     return json.ids;
   } catch (err) {
@@ -28,32 +32,44 @@ function getYoutubeVideoIds() {
   }
 }
 
-
 async function main() {
-  const youtubeVideoIds = getYoutubeVideoIds();
+  const newVideo = await CheckNewVideo();
 
-  for (const youtubeVideoId of youtubeVideoIds) {
-    const youtubeVideoInfo = await getVideoInfo(youtubeVideoId);
-    const { title, description, uploadDate } = youtubeVideoInfo;
-    console.log(`Titulo: ${title}`);
-    console.log(`Descrição: ${description}`);
-    console.log(`Data de Upload: ${JSON.stringify(uploadDate)}`);
+  if (newVideo) {
 
-    await Promise.all([downloadThumbnail(youtubeVideoId), downloadAudio(youtubeVideoId)]);
+    // UpdateJSON(newVideo);
 
-    console.log(`Postando Episodio ${youtubeVideoId} no AnchorFM`);
-    await postEpisode(youtubeVideoInfo);
+    const youtubeVideoIds = getYoutubeVideoIds();
 
-    console.log(`>>>>> Episodio ${title} postado com sucesso <<<<<`);
+    for (const youtubeVideoId of youtubeVideoIds) {
+      const youtubeVideoInfo = await getVideoInfo(youtubeVideoId);
+      const { title, description, uploadDate } = youtubeVideoInfo;
+      console.log(`Titulo: ${title}`);
+      console.log(`Descrição: ${description}`);
+      console.log(`Data de Upload: ${JSON.stringify(uploadDate)}`);
+
+      await Promise.all([
+        downloadThumbnail(youtubeVideoId),
+        downloadAudio(youtubeVideoId),
+      ]);
+
+      console.log(`Postando Episodio ${youtubeVideoId} no AnchorFM`);
+      await postEpisode(youtubeVideoInfo);
+
+      console.log(`>>>>> Episodio ${title} postado com sucesso <<<<<`);
+    }
+  } else {
+
+    console.log("Nao ha novos videos")
+
   }
 }
 
 main()
   .then(() => {
-    console.log(' >>> Processo finalizado com sucesso <<<');
+    console.log(" >>> Processo finalizado com sucesso <<<");
   })
   .catch((err) => {
     console.error(err);
     exit(1);
   });
-
